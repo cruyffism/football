d7e75a740794db7461a623ff4ed4e0bb0b2ef6c1
tkddk0119 <tkddk0119@wisenut.co.kr>
2023-06-02 09:18:25 +0900

    수정

--- src/main/java/com/minki/football/configuration/SecurityConfig.java
+++ src/main/java/com/minki/football/configuration/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
         http
                 .logout((logout) ->
                         logout
-//                                .logoutSuccessUrl("/")
+                                .logoutSuccessUrl("/")
                                 .permitAll()
                 );
 
