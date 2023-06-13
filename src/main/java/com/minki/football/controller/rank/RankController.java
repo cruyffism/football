99946e1d09477236ebedd823a54a6d07c8f1d0f3
tkddk0119 <tkddk0119@wisenut.co.kr>
2023-06-13 14:43:52 +0900

    수정

--- src/main/java/com/minki/football/controller/rank/RankController.java
+++ src/main/java/com/minki/football/controller/rank/RankController.java
@@ -37,13 +37,13 @@ public class RankController {
 
         model.addAttribute("player", playerRes);
         System.out.println("선수순위 : " + playerRes);
-        return "rank/player"; //league 폴더안에 info.html로 보낸다.
+        return "rank/playerAjax"; //league 폴더안에 info.html로 보낸다.
     }
 
     //랭크페이지 가기
     @GetMapping
     public String rank(Model model) {
         System.out.println("순위페이지" );
-        return "rank/playerAjax";
+        return "rank/rankList";
     }
 }
--- src/main/resources/static/js/rankList.js
+++ src/main/resources/static/js/rankList.js
@@ -8,14 +8,15 @@ function rankListAjax(type) {
     const innerHtml = $("#rankList")
     if (type === 1) {
         $.ajax({
-            url: "/rank/team/1/" + league_id,
+            url: "/rank/team/1",
             type: 'GET',
             cache: false,
             dataType: "html",
             async: false,
             success: function (data) {
-                document.getElementById("rankTab1").classList.remove("active");
+                // document.getElementById("rankTab1").classList.remove("active");
                 document.getElementById("rankTab2").classList.remove("active");
+                document.getElementById("rankTab1").className += " active"
                 $(innerHtml).html(data)
                 setTimeout(function () {
                 }, 1000)
@@ -27,14 +28,15 @@ function rankListAjax(type) {
 
     } else {
         $.ajax({
-            url: "/rank/player" + league_id,
+            url: "/rank/player",
             type: 'GET',
             cache: false,
             dataType: "html",
             async: false,
             success: function (data) {
                 document.getElementById("rankTab1").classList.remove("active");
-                document.getElementById("rankTab2").classList.remove("active");
+                // document.getElementById("rankTab2").classList.remove("active");
+                document.getElementById("rankTab2").className += " active"
                 $(innerHtml).html(data)
                 setTimeout(function () {
                 }, 1000)
