dc3ece1909417e657a22888a12586b93e6a1b02c
tkddk0119 <tkddk0119@wisenut.co.kr>
2023-06-26 09:20:22 +0900

    수정

--- src/main/resources/static/js/boardList.js
+++ src/main/resources/static/js/boardList.js
@@ -1,20 +1,20 @@
 $(document).ready(function () { // 페이지가 로딩되는 순간 바로 실행
     console.log("ready!");
-    boardListAjax(); // 1,"creat_date","DESC",2
+    boardListAjax(1,"creat_date","DESC",2); // 1,"creat_date","DESC",2
 });
 
-function boardListAjax() { // page, sortColumn, sortType, idx
+function boardListAjax(page, sortColumn, sortType, idx) { // page, sortColumn, sortType, idx
     // console.log("page : " + page)
     // console.log("sortColumn : " + sortColumn)
     // console.log("sortType : " + sortType)
     // console.log("idx : " + idx)
     const innerHtml = $("#boardListAjax")
-    // const f = document.getElementById("form1");
-    // f.page.value = page;
-    // f.sortColumn.value = sortColumn;
-    // f.sortType.value = sortType;
-    // f.idx.value = idx;
-    // console.log("page : " + f.page.value)
+    const f = document.getElementById("form1");
+    f.page.value = page;
+    f.sortColumn.value = sortColumn;
+    f.sortType.value = sortType;
+    f.idx.value = idx;
+    console.log("page : " + f.page.value)
 
     $.ajax({
         url: "/board/listAjax",
@@ -45,9 +45,9 @@ function boardListAjax() { // page, sortColumn, sortType, idx
 function sortUpDown(idx) {
     const imgTag = document.getElementById("sort" + idx);
     if ($('#sortType').val() === 'DESC' && imgTag.className === $('#sortColumn').val()) {
-        rankPlayerAjax('1', imgTag.className, 'ASC', idx);
+        boardListAjax('1', imgTag.className, 'ASC', idx);
     } else {
-        rankPlayerAjax('1', imgTag.className, 'DESC', idx);
+        boardListAjax('1', imgTag.className, 'DESC', idx);
     }
 }
 
