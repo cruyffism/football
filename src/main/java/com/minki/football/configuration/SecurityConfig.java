9678d1732c88e77cb0ffb9106a098bdf68b91c57
minki <mkhong916@naver.com>
2023-07-30 20:22:20 +0900

    .
    .

--- src/main/java/com/minki/football/configuration/SecurityConfig.java
+++ src/main/java/com/minki/football/configuration/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         (authorizeHttpRequests) ->
                                 authorizeHttpRequests
-                                        .requestMatchers("/","/admin/**", "/user/**", "/league/**", "/team/**", "/player/**","/board/**", "/rank/**", "/static/js/**", "/static/assets/img/**", "/static/css/**", "/static/images/**").permitAll()
+                                        .requestMatchers("/", "/admin/**", "/user/**", "/league/**", "/team/**", "/player/**", "/board/**", "/rank/**", "/static/js/**", "/static/assets/img/**", "/static/css/**", "/static/images/**").permitAll()
                                         .requestMatchers("/admin/**").hasRole("ADMIN")
                                         .anyRequest().authenticated()
                 );
@@ -47,7 +47,7 @@ public class SecurityConfig {
                                 .loginProcessingUrl("/user/loginProc")
                                 .usernameParameter("username")
                                 .passwordParameter("password")
-                                .defaultSuccessUrl("/user/index",true)
+                                .defaultSuccessUrl("/user/index", true)
                                 .failureHandler(customFailureHandler) // 로그인 실패 핸들러
                                 .permitAll()
                 );
